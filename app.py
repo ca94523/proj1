2d0ed51bb3d5f2ccc321c26828a6835743379e6d
akj_ubuntu <akjanto@gmail.com>
2020-08-31 14:07:47 +0700

    d

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from datetime import datetime
+import re
+
 app = Flask(__name__)
 
 
@@ -6,6 +9,20 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
-# to run in terminal:
-# export FLASK_APP=hello
-# python -m flask run --host=0.0.0.0 --port=80
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
