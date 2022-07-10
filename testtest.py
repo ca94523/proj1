8448935551c6b6116b1d68b095678b1be1851526
ca94523 <ca94523@yahoo.com>
2022-07-10 12:31:09 +0700

    test

--- testtest.py
+++ testtest.py
@@ -14,4 +14,4 @@ print("test8")
 x = np.linspace(0, 20, 100)
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
 plt.show()                   # Display the plot
-#plt.savefig("delete.png")
+plt.savefig("delete.png")
