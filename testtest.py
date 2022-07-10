22523b17e2f57a528c134e8eb77a38f4419fc8b5
ca94523 <ca94523@yahoo.com>
2022-07-10 12:44:44 +0700

    kjkj

--- testtest.py
+++ testtest.py
@@ -13,5 +13,5 @@ print("test8")
 # Create a list of evenly-spaced numbers over the range
 x = np.linspace(0, 20, 100)
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the plot
+#plt.show()                   # Display the plot
 plt.savefig("delete.png")
