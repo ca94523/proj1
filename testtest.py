1d48fc54ce75ceb32e3ae667e5e2fd4f3ddeb7da
ca94523 <ca94523@yahoo.com>
2022-07-13 07:06:38 +0700

    sdfsdf

--- testtest.py
+++ testtest.py
@@ -1,12 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-import numpy as np
 
 print("test1")
 
 # Create a list of evenly-spaced numbers over the range
 x = np.linspace(0, 20, 100)
-#plt.plot(x, np.sin(x))       # Plot the sine of each x point
-#plt.show()                   # Display the plot
-#plt.savefig("delete.png")
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+# plt.show()                   # Display the plot
+plt.savefig("delete.png")
